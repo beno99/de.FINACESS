89251c7be7f8ce616360a41d7c5ab321c5c5f3e8
beno99 <benonibrandao2004@gmail.com>
2024-03-08 17:25:53 -0300

    sibindo codigo

---
+++ finances.js
@@ -0,0 +1,12 @@
+let opemModal = document.getElementById("opem-modal");
+let modal = document.querySelector("dialog");
+let cancelModal = document.getElementById("cancelar-bnt");
+let salvar = document.getElementById("salvar-bnt"); 
+
+opemModal.onclick = function(){
+    modal.showModal()
+};
+
+cancelModal.salvar.onclick = function(){
+    modal.close()
+};
